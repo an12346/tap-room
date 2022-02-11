da36768d4c7684c297227786eb3f804846a079dc
Anna Pittman <annaepittman@gmail.com>
2022-02-11 11:19:35 -0800

    create handleClick method

--- src/components/KombuchaControl.js
+++ src/components/KombuchaControl.js
@@ -1,1 +1,31 @@
 import React from "react";
+import NewKombuchaForm from "./NewKombuchaForm";
+import KombuchaList from "./KombuchaList";
+
+class KombuchaControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      kombuchaFormVisibeOnPage: false,
+      mainKombuchaList: []
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      kombuchaFormVisibleOnPage: !prevState.kombuchaFormVisibleOnPage
+    }));
+  }
+
+  //0 => KombuchaList
+  //1 => 
+
+  render(){
+    let currentState = null;
+    let buttonText = null;
+    if (this.state.kombuchaPageShowing === 0) {
+      currentState = < 
+    }
+  }
+}
