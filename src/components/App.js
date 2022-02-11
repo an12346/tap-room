2090ec8e945d0952dd6961629d81a5b9bd089d67
Anna Pittman <annaepittman@gmail.com>
2022-02-11 12:14:58 -0800

    update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,29 @@ import React from "react";
 import Header from "./Header";
 import KombuchaControl from "./KombuchaControl";
 
-function App(){
-  return (
-    <React.Fragment>
-      <Header />
-      <KombuchaControl />
-    </React.Fragment>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      kombuchaMenu: []
+    };
+  }
+
+  handleMenuAddition = (newKombucha) => {
+    const placeholder = this.state.kombuchaMenu.concat(newKombucha);
+    this.setState({
+      kombuchaMenu: placeholder
+    });
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        <Header />
+        <KombuchaControl onCartAddition={this.handleMenuAddition} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
