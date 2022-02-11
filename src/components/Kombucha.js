7bfe1dc2242bc2ccf697ade4eb110989c0951973
Anna Pittman <annaepittman@gmail.com>
2022-02-11 11:41:50 -0800

    edit Kombucha.js

--- src/components/Kombucha.js
+++ src/components/Kombucha.js
@@ -2,13 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Kombucha(props){
+  let message;
+  if(props.content.quantity < 1){
+    message = "out of stock";
+  } else {
+    message = <button onClick={()=> props.method(props.position)}>Pint Sold</button>
+  }
   return (
     <React.Fragment>
-        <h2>{props.name}</h2>
+      <div key={props.content.id}>
+        <h2>{props.content.name}</h2>
         <h4>{props.content.brand}</h4>
         <h4>{props.content.price}</h4>
         <h4>{props.content.flavor}</h4>
         <h4>{props.content.quantity}</h4>
+      </div>
     </React.Fragment>
   )
 }
