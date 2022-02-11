1202304917942522217ab2c18f9dd5fab26118db
Anna Pittman <annaepittman@gmail.com>
2022-02-11 11:33:52 -0800

    edit KombuchaControl.js

--- src/components/KombuchaControl.js
+++ src/components/KombuchaControl.js
@@ -7,7 +7,7 @@ class KombuchaControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      kombuchaFormVisibeOnPage: false,
+      kombuchaFormVisibleOnPage: false,
       mainKombuchaList: []
     };
   }
@@ -18,14 +18,42 @@ class KombuchaControl extends React.Component {
     }));
   }
 
+  handleAddingNewKombuchaToList = (newKombucha) => {
+    const newMainKombuchaList = this.state.mainKombuchaList.concat(newKombucha);
+    this.setState({
+      mainKombuchaList: newMainKombuchaList,
+      kombuchaFormVisibleOnPage: false
+    });
+  }
+
+  handleDecrementKombuchaQuantity = (quantityKombucha) => {
+    const updateKombuchaQuantity = [...this.state.mainKombuchaList];
+    updateKombuchaQuantity[quantityKombucha].quantity = updateKombuchaQuantity[quantityKombucha].quantity - 1;
+    this.setState({
+      mainKombuchaList: updateKombuchaQuantity
+    })
+  }
+
   //0 => KombuchaList
   //1 => 
 
   render(){
     let currentState = null;
     let buttonText = null;
-    if (this.state.kombuchaPageShowing === 0) {
-      currentState = < 
+    if (this.state.kombuchaFormVisibleOnPage) {
+      currentState = <NewKombuchaForm onNewKombuchaCreation={this.handleAddingNewKombuchaToList} />;
+      buttonText = "Go back to Kombucha Menu"; 
+    } else {
+      currentState = <KombuchaList onDecrementKombuchaQuantity={this.handleDecrementKombuchaQuantity} kombuchaList={this.state.mainKombuchaList} />;
+      buttonText = "Add Kombucha";
     }
+    return (
+      <React.Fragment>
+        {currentState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
   }
 }
+
+export default KombuchaControl;
